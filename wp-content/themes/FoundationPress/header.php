16765fd4049ff8079f60a54f254861aa011d4ce3
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-07-10 11:46:16 +0300

    prevent access to single theme group apages

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -11,45 +11,54 @@
 ?>
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-	<div class="off-canvas-wrapper">
-		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
+		<div class="off-canvas-wrapper">
+			<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
+		<?php endif; ?>
 
-	<?php do_action( 'foundationpress_layout_start' ); ?>
+		<?php do_action( 'foundationpress_layout_start' ); ?>
 
-	<header class="site-header" role="banner">
-		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
-			<div class="title-bar-left">
-				<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</span>
-			</div>
-		</div>
-		<nav class="site-navigation top-bar" role="navigation">
-			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+		<header class="site-header" role="banner">
+			<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
+				<div class="title-bar-left">
+					<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+					<span class="site-mobile-title title-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</span>
 				</div>
 			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
+			<nav class="site-navigation top-bar" role="navigation">
+				<div class="top-bar-left">
+					<div class="site-desktop-title top-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</div>
+				</div>
+				<div class="top-bar-right">
+					<?php foundationpress_top_bar_r(); ?>
 
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</div>
-		</nav>
-	</header>
+					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+					<?php endif; ?>
+				</div>
+			</nav>
+		</header>
 
-	<section class="container">
-		<?php do_action( 'foundationpress_after_header' );
+		<section class="container">
+			<?php do_action( 'foundationpress_after_header' );
+
+			// prevent access to "theme group pages", since we're not going to have those
+			if( is_single() && 'theme_group' == get_post_type()	) {
+				global $wp_query;
+				$wp_query->set_404();
+				status_header( 404 );
+				get_template_part( '404' );
+				exit();
+			}
