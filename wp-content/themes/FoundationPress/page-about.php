976a7cdc18c546f65a053f931f5fdd30d191f979
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-07-12 14:37:31 +0300

    display team members

--- wp-content/themes/FoundationPress/page-about.php
+++ wp-content/themes/FoundationPress/page-about.php
@@ -44,11 +44,34 @@ get_header(); ?>
 			</article>
 		<?php endwhile;?>
 
-		<section class="core-team">
-		
+		<section class="team-members">
+			<?php
+
+			//Get all team members
+			$team_members_array = get_posts(
+				array( 'showposts' => -1,
+					'post_type' => 'team_members'
+					)
+				);
+			
+			//Display all team members
+			foreach ($team_members_array as $team_member){
+				$custom_fields = CFS()->get(false, $team_member->ID);
+
+				?>
+				<div class="team-member">
+				<img src="<?php echo get_the_post_thumbnail_url($team_member->ID, 'thumbnail');?>"></img>
+				<span><?php echo $custom_fields["name"]; ?></span>
+				<span><?php echo $custom_fields["title"]; ?></span>
+				<span><?php echo $custom_fields["contact_info"]; ?></span>
+				<span><a href="<?php echo $custom_fields["social_media"]["url"];?>"><?php echo $custom_fields["social_media"]["text"]; ?></a></span>
+				</div>
+				<?php 
+			}
+			?>
 		</section>
 
-<?php 
+		<?php 
 global $post; // required
 $args = array('category' => 8); // include category 8 (About)
 $custom_posts = get_posts($args);
@@ -57,14 +80,14 @@ foreach($custom_posts as $post) {
 
 	?>
 	<article class="about-aole-article">
-	<h2><?php the_title(); ?></h2>
-	<p><?php the_content(); ?></p>
+		<h2><?php the_title(); ?></h2>
+		<p><?php the_content(); ?></p>
 	</article>
 
 	<?php } // end foreach ?>
 
-<?php do_action( 'foundationpress_after_content' ); ?>
-<?php get_sidebar(); ?>
+	<?php do_action( 'foundationpress_after_content' ); ?>
+	<?php get_sidebar(); ?>
 
 </div>
 
