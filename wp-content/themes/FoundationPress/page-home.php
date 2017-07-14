7b305708c92aa1c6184ee15d297184f8d1fb8fec
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-07-14 12:50:35 +0300

    praise the lord, i finally got my admin accounts. which means sass is now working. which means that i did the initial layout minus showcases, for the front page.

--- wp-content/themes/FoundationPress/page-home.php
+++ wp-content/themes/FoundationPress/page-home.php
@@ -3,64 +3,90 @@
 Template Name: Front
 */
 get_header(); ?>
-
-
-
 <?php 
 do_action( 'foundationpress_before_content' ); 
 
 $fields = CFS()->get();
-print_r($fields); 
+//print_r($fields); 
 ?>
-
-<section class="vrbox">
-	<blockquote data-width="100%" data-height="600px" class="ricoh-theta-spherical-image" >Post from RICOH THETA. #theta360 - <a href="https://theta360.com/s/jFtMPIK0W44acnaWi1y0nrwrg" target="_blank">Spherical Image - RICOH THETA</a></blockquote>
-	<script async src="https://theta360.com/widgets.js" charset="utf-8"></script>
-</section>
-<section class="aole-info front-section">
-	<div class="what-is-aole front-section">
-		<h2><?php echo $fields["what_is_aole_title"]; ?></h2>
-		<p><?php echo $fields["what_is_aole_content"]; ?></p>
+<div class="main-wrap front-page full-width" role="main">
+	<section class="vrbox">
+		<blockquote data-width="100%" data-height="600px" class="ricoh-theta-spherical-image" >Post from RICOH THETA. #theta360 - <a href="https://theta360.com/s/jFtMPIK0W44acnaWi1y0nrwrg" target="_blank">Spherical Image - RICOH THETA</a></blockquote>
+		<script async src="https://theta360.com/widgets.js" charset="utf-8"></script>
+	</section>
+	<section class="aole-info-container front-box">
+		<div class="aole-info">
+			<div class="aole-description">
+				<h2><?php echo $fields["what_is_aole_title"]; ?></h2>
+				<p><?php echo $fields["what_is_aole_content"]; ?></p>
+			</div>
+		</div>
+	</section>
+	<section class="teaching-container front-box">
+		<div class="teaching">
+			<div>
+				<h2><?php echo $fields["call_for_ideas_title"]; ?></h2>
+				<p><?php echo $fields["call_for_ideas_content"]; ?></p>
+			</div>
+		</div>
+	</section>
+	<div class="aole-feed-container">
+		<div class="aole-feed front-box2">
+			<div>
+				<h2><?php echo $fields["news_title"]; ?></h2>
+				<p>selityksiä.</p>
+			</div> 
+		</div>
+		<div class="next-event front-box2">
+			<div>
+				<h2>Next event</h2>
+				<p>Tapahtumapuheita.</p>
+			</div>
+		</div>
 	</div>
-	<div class="teaching-at-aalto front-section">
-		<h2><?php echo $fields["call_for_ideas_title"]; ?></h2>
-		<p><?php echo $fields["call_for_ideas_content"]; ?></p>
-	</div>
-</section>
-<section class="aole-feeds front-section">
-	<div class="aole-feed front-section">
-		<h2><?php echo $fields["news_title"]; ?></h2>
-		<div></div>
-	</div>
-	<div class="next-event front-section">
-		<h2>Next event</h2>
-		<div></div>
-	</div>
-</section>
-<section class="pilots-showcase front-section">
-	<h2><?php echo $fields["pilots_showcase_title"]; ?></h2>
-	<div class="pilots-carousel"></div>
-</section>
-<section class="why-online front-section">
-	<h2><?php echo $fields["why_online_title"]; ?></h2>
-	<p><?php echo $fields["why_online_content"]; ?></p>
-</section>
-<section class="why-aole front-section">
-	<h2><?php echo $fields["why_aole_title"]; ?></h2>
-	<p><?php echo $fields["why_aole_content"]; ?></p>
-</section>
 
 
-<?php do_action( 'foundationpress_after_content' ); ?>
+<!--	<section class="pilots-showcase front-section">
+		<h2><?php echo $fields["pilots_showcase_title"]; ?></h2>
+		<div class="pilots-carousel"></div>
+	</section> -->
 
-<div class="section-divider">
-	<hr />
+
+	<section class="why-online-container">
+		<div class="why-online">
+			<div class="content front-box3">
+				<h2><?php echo $fields["why_online_title"]; ?></h2>
+				<p><?php echo $fields["why_online_content"]; ?></p>
+			</div>
+			<div class="image front-box3">
+				KUVA
+			</div>
+		</div>
+	</section>
+	<section class="why-aole-container">
+		<div class="why-aole">
+			<div class="content front-box4">
+				<h2><?php echo $fields["why_aole_title"]; ?></h2>
+				<p><?php echo $fields["why_aole_content"]; ?></p>
+			</div>
+			<div class="image front-box4">
+				KUVA
+			</div>
+		</div>
+	</section>
+
+
+	<?php do_action( 'foundationpress_after_content' ); ?>
+
+	<div class="section-divider">
+		<hr />
+	</div>
+
+
+	<section class="benefits">
+
+	</section>
 </div>
-
-
-<section class="benefits">
-	
-</section>
 
 
 
