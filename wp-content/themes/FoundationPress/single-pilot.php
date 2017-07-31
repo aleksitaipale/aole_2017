b3ff50fcd262e07224906a6eb7399b0fad211a77
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-07-31 13:44:15 +0300

    single pilot page structure

--- wp-content/themes/FoundationPress/single-pilot.php
+++ wp-content/themes/FoundationPress/single-pilot.php
@@ -8,26 +8,35 @@
 
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
 
-<div class="main-wrap" role="main">
+<div class="main-wrap single-pilot-page full-width" role="main">
 
+	<div class="pilot-image-container">
+		<div class="pilot-image matched-height">
+			<div><?php the_post_thumbnail();?></div>
+		</div>
+	</div>
 	<?php do_action( 'foundationpress_before_content' ); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+		<article <?php post_class('main-content pilot-description-container matched-height') ?> id="post-<?php the_ID(); ?>">
+			<header class="pilot-description">
 				<?php foundationpress_entry_meta(); ?>
-				<div class="pilot-description">
+				<div class="pilot-description-content">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
 					<h2>Description</h2>
 					<?php echo CFS()->get( 'description' ); ?>
 				</div>
 			</header>
+		</article>
+
+		<div class="content-container">
+
 			<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+
 			<div class="entry-content">
 
 				<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-				<section class="pilot-info">
+				<section class="pilot-info matched-height2">
 					<div class="">
 						<h2>People</h2>
 						<p><?php echo CFS()->get( 'people' ); ?></p>
@@ -41,14 +50,11 @@ get_header(); ?>
 						<p><?php echo CFS()->get( 'links_materials' ); ?></p>
 					</div>
 				</section>
-
-
-
-
-				<section class="pilot-reflection">
+				<section class="pilot-reflection matched-height2">
 					<h2>Reflection</h2>
 					<p><?php echo CFS()->get( 'reflection' ); ?></p>
 				</section>
+
 			</div>
 
 
@@ -67,7 +73,7 @@ get_header(); ?>
 				<?php do_action( 'foundationpress_post_before_comments' ); ?>
 				<?php comments_template(); ?>
 				<?php do_action( 'foundationpress_post_after_comments' ); ?>
-			</article>
+			</div>
 		<?php endwhile;?>
 
 		<?php do_action( 'foundationpress_after_content' ); ?>
