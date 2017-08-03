fdb0394a464c86db1f0480d2bea1e4b352f8ef0c
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-03 18:07:39 +0300

    single pages

---
+++ wp-content/themes/FoundationPress/single-pilot.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+
+<div class="main-wrap single-pilot-page full-width" role="main">
+	<div class="pilot-image-container">
+		<div class="pilot-image matched-height">
+			<div><?php the_post_thumbnail();?></div>
+		</div>
+	</div>
+	<?php do_action( 'foundationpress_before_content' ); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article <?php post_class('main-content pilot-description-container matched-height') ?> id="post-<?php the_ID(); ?>">
+			<header class="pilot-description">
+				<div class="pilot-description-content">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<h2>Description</h2>
+					<?php the_field( 'description' ); ?>
+					<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+				</div>
+			</header>
+		</article>
+
+		<div class="content-container">
+			<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+			<div class="entry-content">
+				<section class="pilot-info matched-height2">
+					<div>
+						<h2>People</h2>
+						<p><?php the_field( 'people' ); ?></p>
+					</div>
+					<div>
+						<h2>Tools used</h2>
+						<p><?php the_field( 'tools_used' ); ?></p>
+					</div>
+					<div>
+						<h2>Links and materials</h2>
+						<p><?php the_field( 'links_materials' ); ?></p>
+					</div>
+				</section>
+				<section class="pilot-reflection matched-height2">
+					<h2>Reflection</h2>
+					<p><?php the_field( 'reflection' ); ?></p>
+				</section>
+			</div>
+
+
+			<footer>
+				<?php
+				wp_link_pages(
+					array(
+						'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
+						'after'  => '</p></nav>',
+						)
+					);
+					?>
+					<p><?php the_tags(); ?></p>
+				</footer>
+				<?php the_post_navigation(); ?>
+				<?php do_action( 'foundationpress_post_before_comments' ); ?>
+				<?php comments_template(); ?>
+				<?php do_action( 'foundationpress_post_after_comments' ); ?>
+			</div>
+		<?php endwhile;?>
+
+		<?php do_action( 'foundationpress_after_content' ); ?>
+		<?php get_sidebar(); ?>
+	</div>
+	<?php get_footer();
