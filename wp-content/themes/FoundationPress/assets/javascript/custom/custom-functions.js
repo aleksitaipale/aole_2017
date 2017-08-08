04195b03d2a6cb341a529c5e3314f6135ba0f3dc
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-08 17:32:27 +0300

    font size changes in day event listing

--- wp-content/themes/FoundationPress/assets/javascript/custom/custom-functions.js
+++ wp-content/themes/FoundationPress/assets/javascript/custom/custom-functions.js
@@ -6,7 +6,7 @@ jQuery(".event-calendar").on( "click", ".eventful button", function(){
 
 	// Clear the div first
 	$("#day-event-list").html("");
-	$("#day-event-list").append("<h3>"+date+"</h3>");
+	$("#day-event-list").append("<h5>"+date+"</h5>");
 
 	for (var i in events){
 		var eventTitle = "<a href='"+events[i].permalink+"'><span class='day-event-title'>"+events[i].event.event_name+"</span></a>";
