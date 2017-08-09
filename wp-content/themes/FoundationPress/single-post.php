b8db95aea60c1772615a6075b8f27c916e4bd2ad
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-09 10:43:16 +0300

    single post page, fixes #26

--- wp-content/themes/FoundationPress/single-post.php
+++ wp-content/themes/FoundationPress/single-post.php
@@ -1,6 +1,10 @@
-<?php
+ <?php
 /**
- * The template for displaying all single posts and attachments
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
  *
  * @package FoundationPress
  * @since FoundationPress 1.0.0
@@ -8,41 +12,48 @@
 
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
 
-<div class="main-wrap" role="main">
 
+
+
+
+<div class="main-wrap single-post-page" role="main">
 	<?php do_action( 'foundationpress_before_content' ); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h2 class="entry-title"><?php the_title(); ?></h2>
-				<?php foundationpress_entry_meta(); ?>
-			</header>
-			<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
-				<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-			</div>
-			<footer>
-				<?php
-				wp_link_pages(
-					array(
-						'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-						'after'  => '</p></nav>',
-						)
-					);
-					?>
-					<p><?php the_tags(); ?></p>
-				</footer>
-				<?php the_post_navigation(); ?>
-				<?php do_action( 'foundationpress_post_before_comments' ); ?>
-				<?php comments_template(); ?>
-				<?php do_action( 'foundationpress_post_after_comments' ); ?>
-			</article>
-		<?php endwhile;?>
 
-		<?php do_action( 'foundationpress_after_content' ); ?>
-		<?php get_sidebar(); ?>
-	</div>
-	<?php get_footer();
+
+		<div class="post-container">
+			<div class="post">
+				<?php do_action( 'foundationpress_before_content' ); ?>
+				<?php if(has_post_thumbnail()): ?>
+					<div class="post-image-container">
+						<div class="post-image">
+							<?php the_post_thumbnail(); ?>
+						</div>
+					</div>
+				<?php endif; ?>
+				<div class="post-content">
+					<span class="the-author">
+						<?php the_author(); ?>
+					</span>
+					<h2><?php the_title(); ?></h2>
+					<?php the_content(); ?>
+
+					<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+
+				</div>
+			</div>
+		</div>
+
+
+
+
+	<?php endwhile;?>
+
+	<?php do_action( 'foundationpress_after_content' ); ?>
+	<?php get_sidebar(); ?>
+
+</div>
+
+
+<?php get_footer();
