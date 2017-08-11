83969aba6a241106e69e013d1fff4858c576610f
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-11 14:55:40 +0300

    mixin for events

--- wp-content/themes/FoundationPress/page-events.php
+++ wp-content/themes/FoundationPress/page-events.php
@@ -149,42 +149,46 @@ get_header(); ?>
 				foreach ($past_events as &$event) {
 					?>
 					<div class="event-container">
-						<div class="event past-event" data-equalizer>
-							<div class="event-left-container" data-equalizer-watch>
+					<div class="event past-event" data-equalizer>
+							<!-- Event categories -->
+							<div class="event-title-container" data-equalizer-watch>
+								<ul class="event-category-list">
+									<?php foreach($event["event"]->event_categories as $cat){ echo "<li>".$cat->name."</li>"; } ?>
+								</ul>
+								<!-- Facilitator(s) -->
+								<ul class="event-facilitator-list">
+									<?php foreach($event["event"]->custom_fields["facilitators"] as $field){ echo "<li>".$field["facilitator"]."</li>"; } ?>
+								</ul>
+								<!-- Event title -->
+								<a class="event-title" href="<?php echo $event["event"]->the_permalink; ?>"><h5><?php echo $event["event"]->event_name; ?></h5></a>	
+							</div>
+							<div class="event-information-container" data-equalizer-watch>
 								<!-- Date -->
-								<span><?php 
+								<span class="event-date"><?php 
 									$start_date = date_create($event["event"]->event_start_date);
 									$end_date = date_create($event["event"]->event_end_date);
 									if ($event->event_start_date != $event->event_end_date){
 										echo date_format($start_date, "D d F")."-".date_format($end_date, "D d F");
 									} else {
 										echo date_format($start_date, "D d F");
-									}	
+									}
 									?></span>
 									<!-- Time -->
-									<div class="event-time"><?php echo $event["event"]->event_start_time."-".$event["event"]->event_end_time; ?></div>
+									<div class="event-time"><?php echo substr($event["event"]->event_start_time, 0, -3)."-".substr($event["event"]->event_end_time, 0, -3); ?></div>
 									<!-- Location -->
 									<div class="event-location"><?php echo $event["event"]->location->location_name; ?></div>
 									<!-- Only for pilots? -->
 									<div class="event-for-pilots"><?php if ($event["event"]->custom_fields["only_for_pilots"] == 1) { echo "Event for pilots"; } else { echo "Public event"; }; ?> </div>
+									<!-- Export event to iCal -->
+									<div class="export-event-to-ical">
+										<a href="<?php echo do_shortcode("[event post_id='".$event["post"]->ID."']#_EVENTICALURL[/event]");?>">Export to iCal</a>
+									</div>
 								</div>
 								<!-- Picture -->
-
-								<div class="event-center-container" data-equalizer-watch>
+								<div class="event-image-container" data-equalizer-watch>
 									<img class="event-thumbnail" src="<?php echo get_event_image_url($event["event"]->post_id, 'square-large'); ?>"></img>
 								</div>
-								<!-- Event categories -->
-								<div class="event-right-container" data-equalizer-watch>
-									<ul class="event-category-list">
-										<?php foreach($event["event"]->event_categories as $cat){ echo "<li>".$cat->name."</li>"; } ?>
-									</ul>
-									<!-- Facilitator(s) -->
-									<ul class="event-facilitator-list">
-										<?php foreach($event["event"]->custom_fields["facilitators"] as $field){ echo "<li>".$field["facilitator"]."</li>"; } ?>
-									</ul>
-									<!-- Event title -->
-									<a class="event-title" href="<?php echo $event["event"]->the_permalink; ?>"><h5><?php echo $event["event"]->event_name; ?></h5></a>	
-								</div>
+
 							</div>
 						</div>
 						<?php } ?>
