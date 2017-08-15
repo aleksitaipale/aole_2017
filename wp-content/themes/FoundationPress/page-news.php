acd7b9f675db15fb63762c01396a9f839a78b0d6
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-15 11:59:36 +0300

    news page similar to others

--- wp-content/themes/FoundationPress/page-news.php
+++ wp-content/themes/FoundationPress/page-news.php
@@ -13,22 +13,22 @@
 get_header(); ?>
 
 
-<div class="main-wrap page-feed" role="main">
+<div class="main-wrap page-feed full-width" role="main">
 
-	<?php do_action( 'foundationpress_before_content' ); ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class('feed-article-container feed-page-header') ?> id="post-<?php the_ID(); ?>">
-			<div class="feed-article">
-				<div class="feed-article-content">
-					<header>
-						<h2 class="entry-title"><?php the_title(); ?></h2>
-					</header>
+	<div data-equalizer>
+		<div class=" left-header-container" data-equalizer-watch>
+			<?php do_action( 'foundationpress_before_content' ); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article <?php post_class('left-header') ?> id="post-<?php the_ID(); ?>">
+					
 					<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-					<div class="entry-content">
+					<div class="entry-content left-header-content">
+						<header>
+							<h2 class="entry-title"><?php the_title(); ?></h2>
+						</header>
 						<?php the_content(); ?>
-						<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+						<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
 					</div>
-
 					<footer>
 						<?php
 						wp_link_pages(
@@ -43,13 +43,23 @@ get_header(); ?>
 						<?php do_action( 'foundationpress_page_before_comments' ); ?>
 						<?php comments_template(); ?>
 						<?php do_action( 'foundationpress_page_after_comments' ); ?>
-					</div>
-					<div class="feed-article-image">
-						<?php the_post_thumbnail(); ?>
-					</div>
+
+					</article>
 				</div>
-			</article>
-		<?php endwhile;?>
+			<?php endwhile;?>
+
+			<section class="right-header-container" data-equalizer-watch>
+				<div class="right-header">
+					<div class="right-header-content">
+						
+						<?php the_post_thumbnail(); ?>
+
+					</div>
+
+				</div>
+			</section>
+		</div>
+
 		<?php 
 		$args = array( 'category_name' => 'feed,news,awards' );
 		$posts = get_posts($args);
@@ -75,6 +85,7 @@ get_header(); ?>
 			</div>
 
 		</div>
+		<div class="patterned-divider-container"><div class="patterned-divider <?php echo get_custom_pattern_class(); ?>"></div>
 
 	<?php endforeach; 
 	wp_reset_postdata();
