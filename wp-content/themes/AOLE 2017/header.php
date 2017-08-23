1e490ccbb56861017d5e274b5b06a3fe71a3dc69
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-23 09:44:01 +0300

    added cookie notifier

--- wp-content/themes/AOLE 2017/header.php
+++ wp-content/themes/AOLE 2017/header.php
@@ -52,6 +52,27 @@ require_once("custom-functions.php"); // For logic needed on multiple pages (e.g
 	</style>
 
 </noscript>
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+<script>
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#338fd4",
+      "text": "#ffffff"
+    },
+    "button": {
+      "background": "transparent",
+      "text": "#ffffff",
+      "border": "#ffffff"
+    }
+  },
+  "content": {
+    "href": "http://www.aalto.fi/en/site/register/"
+  }
+})});
+</script>
 </head>
 <body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
