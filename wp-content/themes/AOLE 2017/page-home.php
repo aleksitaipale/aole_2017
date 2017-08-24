db22aeae50f7a141b8992aaa3a0668408c6f28e6
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-24 09:23:28 +0300

    remove email form temporarily

--- wp-content/themes/AOLE 2017/page-home.php
+++ wp-content/themes/AOLE 2017/page-home.php
@@ -27,58 +27,34 @@ $target_groups_section = get_field('target_groups_section');
 		<div class="headline">
 			<div class="headline-content">
 				<h3><?php echo $what_is_aole_section["what_is_aole_content"]; ?></h3>
-				<?php echo $what_is_aole_section["email_form_introduction"]; ?>
-
-				<!-- Begin MailChimp Signup Form -->
-				<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-				<style type="text/css">
-					#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-				/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-				We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-			</style>
-			<div id="mc_embed_signup">
-				<form action="//aalto.us16.list-manage.com/subscribe/post?u=18b81c9c3228260654c214afb&amp;id=2aaa357741" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					<div id="mc_embed_signup_scroll">
-						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_18b81c9c3228260654c214afb_2aaa357741" tabindex="-1" value=""></div>
-						<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-					</div>
-				</form>
 			</div>
-
-			<!--End mc_embed_signup-->
-
-
-
+			
 		</div>
-		
 	</div>
-</div>
-<div class="target-group-container">
-	<div class="target-groups" data-equalizer>
-		<div class="teachers">
-			<div data-equalizer-watch>
-				<img src="<?php echo $target_groups_section["for_teachers_image"]; ?>" />
-				<h3><?php echo $target_groups_section["for_teachers_title"]; ?></h3>
-				<?php echo $target_groups_section["for_teachers_content"]; ?>
+	<div class="target-group-container">
+		<div class="target-groups" data-equalizer>
+			<div class="teachers">
+				<div data-equalizer-watch>
+					<img src="<?php echo $target_groups_section["for_teachers_image"]; ?>" />
+					<h3><?php echo $target_groups_section["for_teachers_title"]; ?></h3>
+					<?php echo $target_groups_section["for_teachers_content"]; ?>
+				</div>
 			</div>
-		</div>
 
-		<div class="others" >
-			<div data-equalizer-watch>
-				<img src="<?php echo $target_groups_section["for_all_image"]; ?>" />
-				<h3><?php echo $target_groups_section["for_all_title"]; ?></h3>
-				<?php echo $target_groups_section["for_all_content"]; ?>
+			<div class="others" >
+				<div data-equalizer-watch>
+					<img src="<?php echo $target_groups_section["for_all_image"]; ?>" />
+					<h3><?php echo $target_groups_section["for_all_title"]; ?></h3>
+					<?php echo $target_groups_section["for_all_content"]; ?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<div class="patterned-divider-container"><div class="patterned-divider pattern1"></div></div>
-<section class="aole-feed-container">
-	<div class="aole-feeds" data-equalizer>
-		<?php
+	<div class="patterned-divider-container"><div class="patterned-divider pattern1"></div></div>
+	<section class="aole-feed-container">
+		<div class="aole-feeds" data-equalizer>
+			<?php
 
 					$blog_news_title = get_field("blog_news_title"); // this field has to be retrieved before the "loop inside a loop" to be able to be shown
 
