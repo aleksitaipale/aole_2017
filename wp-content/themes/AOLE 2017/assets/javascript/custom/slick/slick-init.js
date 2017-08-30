aba1baa00e271ce7c329c3153fae25134bf3d2d4
Taipale Aleksi <taipala2@t31300-la083.org.aalto.fi>
2017-08-30 13:25:45 +0300

    slick bug

--- wp-content/themes/AOLE 2017/assets/javascript/custom/slick/slick-init.js
+++ wp-content/themes/AOLE 2017/assets/javascript/custom/slick/slick-init.js
@@ -4,8 +4,8 @@ jQuery(document).ready(function($){
 		dots: true,
 		infinite: false,
 		speed: 300,
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 3,
+		slidesToScroll: 3,
 		mobileFirst: true,
 		responsive:[
 		{
